fccbaebeb067e9efb123781d2964b5a3d3445b29
Art <ya.Artriches@ya.ru>
2020-08-16 14:10:25 +0300

    fix: компанента Hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,10 +4,43 @@ import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/social-links"
 
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>Vladimir</h1>
+            <h4>Frontend Developer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
-
-// ...GatsbyImageSharpFluid
