72199a778a115579e92e21ac53c2a6030a0ef980
Art <ya.Artriches@ya.ru>
2020-08-16 14:10:05 +0300

    fix: компанента Services

--- src/components/services.js
+++ src/components/services.js
@@ -3,7 +3,25 @@ import Title from "./title"
 import services from "../constants/services"
 
 const Services = () => {
-  return <h2>services component</h2>
+  return (
+    <section className="section bg-grey">
+      <Title title="Services" />
+      <div className="section-center services-center">
+        {services.map(service => {
+          const { id, icon, title, text } = service
+
+          return (
+            <article key={id} className="service">
+              {icon}
+              <h4>{title}</h4>
+              <div className="underline"></div>
+              <p>{text}</p>
+            </article>
+          )
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default Services
