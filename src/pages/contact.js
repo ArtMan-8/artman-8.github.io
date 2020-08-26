bd58fe8200bc6ab98d0839eb66ee35f33c4b0986
Art <ya.Artriches@ya.ru>
2020-08-26 09:14:21 +0300

    add: компанент Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,41 @@ import React from "react"
 import Layout from "../components/layout"
 
 const Сontact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+          <form action="https://formspree.io/xnqgyere" method="POST">
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-control"
+              ></textarea>
+            </div>
+
+            <button type="submit" className="btn submit-btn">
+              submit
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default Сontact
