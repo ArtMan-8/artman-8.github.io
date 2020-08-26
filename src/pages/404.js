b502347f29cb831b5e921a85e463931c32eeec17
Art <ya.Artriches@ya.ru>
2020-08-26 08:27:52 +0300

    add: компанент Error

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,18 @@ import { Link } from "gatsby"
 import SEO from "../components/seo"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>oops it`s dead end</h1>
+          <Link to="/" className="btn">
+            back home
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
